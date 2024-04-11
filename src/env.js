96642b5de3b38a4db6977522b5ad0c4a0cae481c
Koke Carpintero <kokecar11.dev@gmail.com>
2024-04-11 14:32:33 -0500

    feat: added openpanel analytics

--- src/env.js
+++ src/env.js
@@ -24,6 +24,7 @@ export const env = createEnv({
     ),
     GOOGLE_CLIENT_ID: z.string(),
     GOOGLE_CLIENT_SECRET: z.string(),
+    OPENPANEL_CLIENT_ID: z.string(),
   },
 
   /**
@@ -46,6 +47,7 @@ export const env = createEnv({
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    OPENPANEL_CLIENT_ID: process.env.OPENPANEL_CLIENT_ID,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
